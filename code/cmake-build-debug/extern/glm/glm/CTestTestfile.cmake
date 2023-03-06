7e47fae77692e0c5e3a400e7d2467a54ab0e42e3
Fabian Maier <fabian.maier1@stud-mail.uni-wuerzburg.de>
2023-03-06 17:30:47 +0100

    Removed ignored build files

--- code/cmake-build-debug/extern/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/GameLabIII/28-gl3-maier/code/extern/glm/glm
-# Build directory: C:/GameLabIII/28-gl3-maier/code/cmake-build-debug/extern/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
